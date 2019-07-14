c0c89c1ed12b292f31ffe8baf50a2b22c003b7aa
joao Nequinha <jcnequinha@gmail.com>
2019-07-14 23:53:51 +0100

    added more css

--- src/App.js
+++ src/App.js
@@ -3,44 +3,40 @@ import './App.css';
 
 function App() {
 
-const displayStyle={
-  backgroundColor: "black",
-  color: "white"
-}
+
 
   return (
     <div className="App">
         <div  id="calc-container">
-              <div id="display-container" style= {displayStyle}> display
-              </div>
+              <div id="display-container" > display</div>
               <div className="row" id="row1">
-                  <button id="bttn-clear">AC</button>
-                  <button id="bttn-negpos">+/-</button>
-                  <button id="bttn-percent">%</button>
-                  <button id="bttn-division">÷</button>
+                  <button className="operatorspe" id="bttn-clear">AC</button>
+                  <button className="operatorspe" id="bttn-negpos">+/-</button>
+                  <button className="operatorspe" id="bttn-percent">%</button>
+                  <button className="operator" id="bttn-division">÷</button>
               </div>
               <div  className="row" id="row2">
-                  <button id="bttn-seven">7</button>
-                  <button id="bttn-eight">8</button>
-                  <button id="bttn-nine">9</button>
-                  <button id="bttn-times">x</button>
+                  <button className="numpad" id="bttn-seven">7</button>
+                  <button className="numpad" id="bttn-eight">8</button>
+                  <button className="numpad" id="bttn-nine">9</button>
+                  <button className="operator" id="bttn-times">x</button>
               </div>
               <div  className="row" id="row3">
-                  <button id="bttn-four">4</button>
-                  <button id="bttn-five">5</button>
-                  <button id="bttn-six">6</button>
-                  <button id="bttn-minus">-</button>
+                  <button className="numpad" id="bttn-four">4</button>
+                  <button className="numpad" id="bttn-five">5</button>
+                  <button className="numpad" id="bttn-six">6</button>
+                  <button className="operator" id="bttn-minus">-</button>
               </div>
               <div  className="row" id="row4">
-                  <button id="bttn-one">1</button>
-                  <button id="bttn-two">2</button>
-                  <button id="bttn-three">3</button>
-                  <button id="bttn-plus">+</button>
+                  <button className="numpad" id="bttn-one">1</button>
+                  <button className="numpad" id="bttn-two">2</button>
+                  <button className="numpad" id="bttn-three">3</button>
+                  <button className="operator" id="bttn-plus">+</button>
               </div>
               <div className="row" id="row5">
-                  <button id="bttn-zero">0</button>
-                  <button id="bttn-dot">.</button>
-                  <button id="bttn-equal">=</button>
+                  <button className="numpad" id="bttn-zero">0</button>
+                  <button className="numpad" id="bttn-dot">.</button>
+                  <button className="operator" id="bttn-equal">=</button>
               </div>
         </div>
     </div>
