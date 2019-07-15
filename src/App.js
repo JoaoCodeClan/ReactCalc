8f3316e8fa34aece2fe03493c656e1e218499b52
joao Nequinha <jcnequinha@gmail.com>
2019-07-15 21:23:43 +0100

    added function to change display on pressing numeric bttns

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 
 class App extends React.Component  {
 
-  state= {
-  	displayValue: "0",
-  	waitingOnNext: false,
-  	operator: undefined,
-  	storedInput: undefined
+  state={
+    	displayValue: "0",
+    	waitingOnNext: false,
+    	operator: undefined,
+    	storedInput: undefined
+    }
 
-  }
+  numberInput=(event)=>{
+
+    const{displayValue, waitingOnNext, operator, storedInput }=this.state
+    const bttnValue= event.target.value;
+
+    this.setState({displayValue: bttnValue });
+
+    if(waitingOnNext){
+			this.setState({
+			displayValue: String(bttnValue),
+			waitingOnNext: false
+			})
+	}else{
+    if(displayValue==='0'){
+      this.setState({storedInput: displayValue });
+      this.setState({displayValue: String(bttnValue)});
+    }else{
+      this.setState({storedInput:displayValue});
+      this.setState({displayValue: displayValue + String(bttnValue)})
+    }
+	}
+}
 
 
 render(){
@@ -21,30 +43,30 @@ render(){
                   <button className="operatorspe" id="bttn-clear">AC</button>
                   <button className="operatorspe" id="bttn-negpos">+/-</button>
                   <button className="operatorspe" id="bttn-percent">%</button>
-                  <button className="operator" id="bttn-division">÷</button>
+                  <button className="operator" value="/" id="bttn-division">÷</button>
               </div>
               <div  className="row" id="row2">
-                  <button className="numpad" id="bttn-seven">7</button>
-                  <button className="numpad" id="bttn-eight">8</button>
-                  <button className="numpad" id="bttn-nine">9</button>
-                  <button className="operator" id="bttn-times">x</button>
+                  <button className="numpad" value="7" id="bttn-seven" onClick={this.numberInput}>7</button>
+                  <button className="numpad" value="8" id="bttn-eight" onClick={this.numberInput}>8</button>
+                  <button className="numpad" value="9" id="bttn-nine" onClick={this.numberInput}>9</button>
+                  <button className="operator" value="*" id="bttn-times">x</button>
               </div>
               <div  className="row" id="row3">
-                  <button className="numpad" id="bttn-four">4</button>
-                  <button className="numpad" id="bttn-five">5</button>
-                  <button className="numpad" id="bttn-six">6</button>
-                  <button className="operator" id="bttn-minus">-</button>
+                  <button className="numpad" value="4" id="bttn-four" onClick={this.numberInput}>4</button>
+                  <button className="numpad" value="5" id="bttn-five" onClick={this.numberInput}>5</button>
+                  <button className="numpad" value="6" id="bttn-six" onClick={this.numberInput}>6</button>
+                  <button className="operator" value="-" id="bttn-minus">-</button>
               </div>
               <div  className="row" id="row4">
-                  <button className="numpad" id="bttn-one">1</button>
-                  <button className="numpad" id="bttn-two">2</button>
-                  <button className="numpad" id="bttn-three">3</button>
-                  <button className="operator" id="bttn-plus">+</button>
+                  <button className="numpad" value="1" id="bttn-one" onClick={this.numberInput}>1</button>
+                  <button className="numpad" value="2" id="bttn-two" onClick={this.numberInput}>2</button>
+                  <button className="numpad" value="3" id="bttn-three" onClick={this.numberInput}>3</button>
+                  <button className="operator" value="+" id="bttn-plus">+</button>
               </div>
               <div className="row" id="row5">
-                  <button className="numpad" id="bttn-zero">0</button>
-                  <button className="numpad" id="bttn-dot">.</button>
-                  <button className="operator" id="bttn-equal">=</button>
+                  <button className="numpad" value="0" id="bttn-zero" onClick={this.numberInput}>0</button>
+                  <button className="numpad" value="." id="bttn-dot">.</button>
+                  <button className="operator"  id="bttn-equal">=</button>
               </div>
         </div>
     </div>
