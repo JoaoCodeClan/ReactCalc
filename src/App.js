733a75941d1c084dd94417e76765953affe1ab11
joao Nequinha <jcnequinha@gmail.com>
2019-07-16 00:19:14 +0100

    added some logic to operations, but still needs work. imported maths.js. but need to research on other ways of doing the logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { evaluate } from 'mathjs';
 
 class App extends React.Component  {
 
@@ -11,23 +12,33 @@ class App extends React.Component  {
     }
 
   numberInput=(event)=>{
-    const{displayValue, waitingOnNext}=this.state
-    const bttnValue= event.target.value;
+    const{displayValue, waitingOnNext, storedInput}=this.state
 
-    this.setState({displayValue: bttnValue });
+
+    const bttnValue= event.target.value;
+//     console.log(event);
+// console.log(bttnValue)
+    // this.setState({displayValue: bttnValue });
 
     if(waitingOnNext){
+
 			this.setState({
+      // storedInput: displayValue,
 			displayValue: String(bttnValue),
-			waitingOnNext: false
+			// waitingOnNext: false
+
 			})
+
 	}else{
     if(displayValue==='0'){
       this.setState({storedInput: displayValue });
       this.setState({displayValue: String(bttnValue)});
+
     }else{
+
       this.setState({storedInput:displayValue});
       this.setState({displayValue: displayValue + String(bttnValue)})
+
     }
 	}
 }
@@ -50,7 +61,9 @@ decimalInput=(event)=>{
 
 
 clearDisplay=(event)=>{
-      this.setState({displayValue: "0"})
+      this.setState({displayValue: "0",
+                    storedInput: undefined
+    })
 }
 
 
@@ -70,6 +83,36 @@ const{displayValue }=this.state;
 }
 
 
+operatorInput=(event)=>{
+  const{displayValue, waitingOnNext, operator, storedInput }=this.state;
+
+this.setState({waitingOnNext: true,
+                storedInput: displayValue
+
+              });
+  const operatorClicked = event.target.value;
+   let opResult= null;
+  const  mathsOperation= `${storedInput}${operatorClicked}${displayValue}`;
+ console.log(mathsOperation)
+   if(storedInput&&operatorClicked!=="="&&waitingOnNext===true){
+    opResult=evaluate(mathsOperation);
+    this.setState({displayValue: opResult,
+              storedInput:opResult
+              })
+
+  }else if(storedInput&&operatorClicked==="="){
+    opResult=evaluate(mathsOperation);
+    console.log(opResult);
+    this.setState({displayValue: String(opResult),
+              storedInput:undefined,
+              waitingOnNext: false
+              })
+    mathsOperation=null;
+
+  }
+
+}
+
 
 
 
@@ -83,30 +126,30 @@ render(){
                   <button className="operatorspe" id="bttn-clear" onClick={this.clearDisplay}>AC</button>
                   <button className="operatorspe" id="bttn-negpos" onClick={this.negDisplay}>+/-</button>
                   <button className="operatorspe" id="bttn-percent" onClick={this.percentInput}>%</button>
-                  <button className="operator" value="/" id="bttn-division">÷</button>
+                  <button className="operator" value="/" id="bttn-division" onClick={this.operatorInput}>÷</button>
               </div>
               <div  className="row" id="row2">
                   <button className="numpad" value="7" id="bttn-seven" onClick={this.numberInput}>7</button>
                   <button className="numpad" value="8" id="bttn-eight" onClick={this.numberInput}>8</button>
                   <button className="numpad" value="9" id="bttn-nine" onClick={this.numberInput}>9</button>
-                  <button className="operator" value="*" id="bttn-times">x</button>
+                  <button className="operator" value="*" id="bttn-times" onClick={this.operatorInput}>x</button>
               </div>
               <div  className="row" id="row3">
                   <button className="numpad" value="4" id="bttn-four" onClick={this.numberInput}>4</button>
                   <button className="numpad" value="5" id="bttn-five" onClick={this.numberInput}>5</button>
                   <button className="numpad" value="6" id="bttn-six" onClick={this.numberInput}>6</button>
-                  <button className="operator" value="-" id="bttn-minus">-</button>
+                  <button className="operator" value="-" id="bttn-minus" onClick={this.operatorInput}>-</button>
               </div>
               <div  className="row" id="row4">
                   <button className="numpad" value="1" id="bttn-one" onClick={this.numberInput}>1</button>
                   <button className="numpad" value="2" id="bttn-two" onClick={this.numberInput}>2</button>
                   <button className="numpad" value="3" id="bttn-three" onClick={this.numberInput}>3</button>
-                  <button className="operator" value="+" id="bttn-plus">+</button>
+                  <button className="operator" value="+" id="bttn-plus" onClick={this.operatorInput}>+</button>
               </div>
               <div className="row" id="row5">
                   <button className="numpad" value="0" id="bttn-zero" onClick={this.numberInput}>0</button>
                   <button className="numpad" value="." id="bttn-dot" onClick={this.decimalInput}>.</button>
-                  <button className="operator"  id="bttn-equal">=</button>
+                  <button className="operator"  id="bttn-equal" >=</button>
               </div>
         </div>
     </div>
